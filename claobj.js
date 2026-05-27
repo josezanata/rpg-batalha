0f76d3d8b107c6e8ac220abd4474af84ed146bca
josezanata <jose.krais@escola.pr.gov.br>
2026-05-27 11:53:36 -0300

    Add files via upload

--- claobj.js
+++ claobj.js
@@ -1,9 +1,20 @@
 class heroi{
-    construtor(nome,classe){
+    constructor(nome,classe){
         this.nome = nome;
-        this.classe = classe;
+        this.classe=classe;
     }
 }
 function criarHeroi(){
-    const nomeHeroi = document.getElementById('nomeInput');
-}
+    // Capturando o que o usuário digitou/selecionou na tela
+    const nomeDigitado = document.getElementById('nomeInput').value;
+    const classeSelecionada = document.getElementById('classeSelect').value;
+    // Criando o objeto dinamicamente com os valores dos campos
+    const novoHeroi= new heroi(nomeDigitado, classeSelecionada);
+
+    document.getElementById('resultado').style.display = "block"
+    document.getElementById('dadoHeroi').innerHTML =`
+    nome: <strong>${novoHeroi.nome}</strong><br>
+    <strong>Classe:</strong> ${novoHeroi.classe}
+    `;
+    
+    }
