c7c8b5b4303a1158440a62375eb5a0f9fb121940
josezanata <jose.krais@escola.pr.gov.br>
2026-05-27 09:52:01 -0300

    Add files via upload

---
+++ claobj.js
@@ -0,0 +1,9 @@
+class heroi{
+    construtor(nome,classe){
+        this.nome = nome;
+        this.classe = classe;
+    }
+}
+function criarHeroi(){
+    const nomeHeroi = document.getElementById('nomeInput');
+}
